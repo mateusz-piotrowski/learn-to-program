4d880633733c3ed2c9083903307e151b6146904c
Mateusz Piotrowski <mateusz7piotrowski@gmail.com>
2021-05-30 22:36:44 +0200

    Add program-logger.rb

---
+++ chapter-10/program-logger.rb
@@ -0,0 +1,40 @@
+puts 
+puts "# = = = = = = = = = = "
+puts "# Chapter 10"
+puts "# File: program-logger.rb"
+puts "# = = = = = = = = = = "
+puts
+
+puts 'The Program Logger.'
+puts ''
+
+text_block_beginning = Proc.new do
+  puts '...nesting text'
+  puts '...after beginning'
+  puts ''
+end
+
+text_block_finishing = Proc.new do
+  puts ''
+  puts '...nesting text'
+  puts '...before finishing'
+end
+
+beginning = Proc.new do
+  puts 'Beginning...'
+  text_block_beginning.call
+end
+
+finished = Proc.new do
+  text_block_finishing.call
+  puts '...Finished.'
+end
+
+def log someProc
+  someProc.call
+end
+
+log beginning
+log finished
+
+puts ''
